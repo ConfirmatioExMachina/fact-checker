1361662ec5d2f5bc22fad8f8365ddd9156ea47b3
Berberer <lukas.brandt@veds.de>
2018-01-02 22:42:59 +0100

    Not existing articles will be stored in a blacklist to be ignored in future calls

--- src/cem/helpers/wikipedia.clj
+++ src/cem/helpers/wikipedia.clj
@@ -2,7 +2,14 @@
   (:import (fastily.jwiki.core Wiki)))
 
 (def wikipedia (delay (Wiki. "en.wikipedia.org")))
+(def blacklist (transient #{}))
 
 (defn fetch-summary
   [title]
-  (.getTextExtract @wikipedia title))
+  (if-not (blacklist title)
+    (let [summary (.getTextExtract @wikipedia title)]
+      (if (some? summary)
+        summary
+        (do
+          (conj! blacklist title)
+          nil)))))
