5d79020aa8371f38122cff6a350a7cc80b6ff9bf
Clemens Damke <admin@cortys.de>
2018-01-21 16:38:56 +0100

    fixed neo4j graph import performance regression
    global concepts caused more merge queries.
    those were slow because they did not use the id index because they did not use the :node label.

--- src/cem/db/neo4j.clj
+++ src/cem/db/neo4j.clj
@@ -16,21 +16,22 @@
   "match (n) detach delete n")
 
 (db/defquery ^:private create-node
-  "merge (n:node {id: $data.id})
+  "create (n:node {id: $data.id})
    set n = $data")
 
 (db/defquery ^:private merge-node
   "merge (n:node {id: $data.id})
-   set n = $data")
+   on create set n = $data")
 
 (defn- create-create-edge-query
   [type]
-  (str "create ({id: $from})-[e:" type "]->({id: $to})"))
+  (str "match (from:node {id: $from}), (to:node {id: $to})
+        create (from)-[: " type "]->(to)"))
 
 (defn- create-merge-edge-query
   [type]
-  (str "match (from {id: $from}), (to {id: $to})
-        merge (from)-[e:" type "]->(to)"))
+  (str "match (from:node {id: $from}), (to:node {id: $to})
+        merge (from)-[: " type "]->(to)"))
 
 (db/defquery ^:private node-types
   "match (n {group: 'concept'})
@@ -66,21 +67,26 @@
    (db/with-retry [@db tx]
      (insert-graph! tx g names)))
   ([tx g names]
+   ; Add nodes:
    (doseq [node (graph/nodes g)
            :let [id (names node)]]
-     ((if (id/concept? id)
-        merge-node create-node)
+     ((if (id/concept? id) merge-node create-node)
       tx {:data (-> (attr/attrs g node)
-                    (dissoc :annotation)
+                    (select-keys [:id :group :label :named
+                                  :negative :possibility
+                                  :absolute-negative
+                                  :root :global])
                     (assoc :id id))}))
+   ; Add edges:
    (doseq [[from to :as edge] (graph/edges g)
            :let [{:keys [type]} (attr/attrs g edge)
                  from (names from)
                  to (names to)]]
-     (db/execute tx ((if (and (id/concept? from) (id/concept? to))
-                       create-merge-edge-query
-                       create-create-edge-query)
-                     type)
+     (db/execute tx
+                 ((if (and (id/concept? from) (id/concept? to))
+                    create-merge-edge-query
+                    create-create-edge-query)
+                  type)
                  {:from (names from), :to (names to)}))
    (println (str "Added graph: "
                  (count (graph/nodes g)) " nodes, "
