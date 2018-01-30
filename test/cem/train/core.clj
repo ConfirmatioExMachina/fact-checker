8fd4d8a52869b72d341fb53832740e3fd0533076
Clemens Damke <admin@cortys.de>
2018-01-30 18:01:36 +0100

    added evaluation code and its results

--- test/cem/train/core.clj
+++ test/cem/train/core.clj
@@ -1,6 +1,8 @@
 (ns cem.train.core
   (:require [clojure.java.io :refer [reader resource]]
-            [clojure.data.csv :refer [read-csv]]))
+            [clojure.string :as str]
+            [clojure.data.csv :refer [read-csv]]
+            [cem.checker.core :as checker]))
 
 (defn read-data
   [path]
@@ -10,8 +12,52 @@
          (map (fn [[id fact truth]]
                 {:id id
                  :fact fact
-                 :truth (Double/parseDouble truth)})))))
+                 :truth (when truth (Double/parseDouble truth))})))))
 
-(def data (delay (read-data (resource "train.tsv"))))
+(defn check-data
+  [data]
+  (pmap (fn [{:keys [id fact truth]}]
+          (println "Checking" id)
+          (let [result (checker/check-fact fact)]
+            (println "Checked" id "with truth" truth "and result" result)
+            [id fact truth result]))
+        data))
 
-(println (take 10 @data))
+(defn results->ttl
+  [results]
+  (str/join (map (fn [[id fact truth result]]
+                   (str "<http://swc2017.aksw.org/task2/dataset/" id "> "
+                        "<http://swc2017.aksw.org/hasTruthValue> "
+                        "\"" result "\"^^<http://www.w3.org/2001/XMLSchema#double> .\n"))
+                 results)))
+
+(defn write-results
+  [results path]
+  (spit path (results->ttl results)))
+
+(defn binarify-result
+  [threshold result]
+  (if (<= threshold result) 1 0))
+
+(defn ROC
+  [threshold checks]
+  (let [binary-checks (map (fn [[id fact truth result]]
+                             [truth (binarify-result threshold result)])
+                           checks)
+        p (remove (fn [[truth result]] (zero? truth)) binary-checks)
+        tp (remove (fn [[truth result]] (zero? result)) p)
+        f (filter (fn [[truth result]] (zero? truth)) binary-checks)
+        fp (remove (fn [[truth result]] (zero? result)) f)
+        tpr (/ (count tp) (count p))
+        fpr (/ (count fp) (count f))]
+    {:tpr tpr, :fpr fpr}))
+
+(defn ROC-curve
+  [results]
+  (set (map #(ROC % (take 60 results))
+            (range -1 1.01 0.01))))
+
+(def train-data (delay (read-data (resource "train.tsv"))))
+(def test-data (delay (read-data (resource "test.tsv"))))
+(def train-results (delay (check-data @train-data)))
+(def test-results (delay (check-data @test-data)))
